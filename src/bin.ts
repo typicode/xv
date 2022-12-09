17ca0d7f13cd5a95d191c02230d2e95cb7faa730
typicode <typicode@gmail.com>
2022-12-09 09:56:14 +0100

    feat: improve typescript support

--- src/bin.ts
+++ src/bin.ts
@@ -1,7 +1,33 @@
 #!/usr/bin/env node
-import { promises as fs } from 'fs'
-import { basename, join } from 'path'
-import { pathToFileURL } from 'url'
+import { fork } from 'node:child_process'
+import { promises as fs } from 'node:fs'
+import { basename, join } from 'node:path'
+import { fileURLToPath, pathToFileURL } from 'node:url'
+
+const regexp = /^test\.(js|ts)$|\.test\.(js|ts)$/
+
+function parse(argv: string[]): [string[], string[]] {
+  const execArgv: string[] = []
+  const args = argv.filter((arg) => {
+    if (arg.startsWith('--')) {
+      execArgv.push(arg)
+      return false
+    }
+    return true
+  })
+  return [execArgv, args]
+}
+
+function forkProcess(execArgv: string[], args: string[]) {
+  const child = fork(fileURLToPath(import.meta.url), args, {
+    execArgv,
+    stdio: 'inherit',
+  })
+  child.on('exit', (code) => process.exit(code ?? 0))
+  child.on('error', (err) => {
+    throw err
+  })
+}
 
 async function* walk(dir: string): AsyncGenerator<string> {
   for await (const d of await fs.opendir(dir)) {
@@ -13,6 +39,7 @@ async function* walk(dir: string): AsyncGenerator<string> {
 
 async function runTestFile(file: string): Promise<void> {
   for (const value of Object.values(
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     await import(pathToFileURL(file).toString()),
   )) {
     if (typeof value === 'function') {
@@ -26,19 +53,24 @@ async function runTestFile(file: string): Promise<void> {
   }
 }
 
-async function run(arg = '.') {
-  if ((await fs.lstat(arg)).isFile()) {
-    return runTestFile(arg)
-  }
-  for await (const file of walk(arg)) {
-    if (
-      basename(file) === 'test.js' ||
-      file.endsWith('.test.js')
-    ) {
-      console.log(file)
-      await runTestFile(file)
+async function run(paths: string[]) {
+  for (const p of paths) {
+    if ((await fs.lstat(p)).isFile()) {
+      return runTestFile(p)
+    }
+    for await (const file of walk(p)) {
+      if (regexp.test(basename(file))) {
+        console.log(file)
+        await runTestFile(file)
+      }
     }
   }
 }
 
-void run(process.argv[2])
+const [execArgv, args] = parse(process.argv.slice(2))
+if (!args.length) args.push('.')
+if (execArgv.length) {
+  forkProcess(execArgv, args)
+} else {
+  void run(args)
+}
